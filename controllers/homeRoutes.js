5197569b4c5a0dd95b6bfe42a672d8b9260965a6
Carl Benbow <cbenbow@synergisticagility.com>
2021-06-13 13:25:36 -0400

    ain handlebar and login handlebar is working, sign in and goes to next route, sucessful

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,9 +6,12 @@ const withAuth = require('../utils/auth');
 router.get('/', async(req, res) => {
 
     try {
+        // const userData = await BlogPost.findAll({})
+
 //review the handlebars ****
-        res.render('homepage', {
+        res.render('login', {
             logged_in: req.session.logged_in
+            // userData
         });
 
     } catch (err) {
@@ -17,9 +20,9 @@ router.get('/', async(req, res) => {
 
 });
 
-router.get('/blogData', withAuth, async(req, res) => {
+router.get('/blogs', withAuth, async(req, res) => {
     try {
-        // Get all stocks based on logged in user
+        // Get all blogs based on logged in user
         const userData = await BlogPost.findAll({
             where: {
                 user_id: req.session.user_id,
@@ -28,7 +31,7 @@ router.get('/blogData', withAuth, async(req, res) => {
 
 
         // Serialize stock data so templates can read it
-        const users = userData.map((stock) => { stock.get({ plain: true }) });
+        const users = userData.map((blogPost) => { blogPost.get({ plain: true }) });
 
         // Pass serialized data into stockcarddetails template
         //update handlebars*****
