b36c8809f116410daf0d759f5e07e27a730a31ab
Carl Benbow <cbenbow@synergisticagility.com>
2021-06-19 15:44:54 -0400

    delete button works

---
+++ public/js/deletePost.js
@@ -0,0 +1,21 @@
+const deletePostHandler = async (event) => {
+    event.preventDefault();
+  
+         id = document.querySelectorAll('div[id]')[1].id
+  
+    if (id) {
+      const response = await fetch(`/api/blog/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/blogs');
+      } else {
+        alert('Failed to delete post');
+      }
+    }
+  };
+  
+  document
+  .querySelector('.deleteBtn')
+  .addEventListener('click', deletePostHandler);
