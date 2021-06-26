191b8355dfba84105f0f375563748e64f55327f2
Carl Benbow <cbenbow@synergisticagility.com>
2021-06-26 12:20:21 -0400

    update nav

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -17,7 +17,7 @@ router.get("/", async (req, res) => {
   }
 });
 
-///FIX
+
 router.get("/login", (req, res) => {
   
   res.render("login");
@@ -34,6 +34,7 @@ router.get("/homepage", withAuth, async (req, res) => {
     });
   } catch (err) {
     res.status(500).json(err);
+    
   }
 });
 
--- utils/auth.js
+++ utils/auth.js
@@ -8,4 +8,5 @@ const withAuth = (req, res, next) => {
   }
 };
 
+
 module.exports = withAuth;
